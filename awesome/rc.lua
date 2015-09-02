34a0a74d39b8323d5a5ab231601413a2eb5e3e25
hackrole <daipeng123456@gmail.com>
2015-09-02 08:46:57 +0800

    add awesome shadown setup

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -142,6 +142,7 @@ mytasklist.buttons = awful.util.table.join(
 
 -- auto run
 awful.util.spawn_with_shell('fcitx -d')
+awful.util.spawn_with_shell('/home/daipeng/app/shadowsocks/shadowsocks-local-linux64-1.1.1 -c /home/daipeng/app/shadowsocks/config.json')
 awful.util.spawn_with_shell('xmodmap /home/daipeng/.xmodmap')
 awful.util.spawn_with_shell('guake')
 awful.util.spawn_with_shell('xscreensaver -no-splash')
