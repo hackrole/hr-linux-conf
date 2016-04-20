35f827dca7a0f2ad36e9e1beb37adef24bf45078
hackrole <daipeng123456@gmail.com>
2016-04-20 16:42:39 +0800

    fix i3 feh_bg and scrot bind

--- feh_bg.sh
+++ feh_bg.sh
@@ -2,6 +2,6 @@
 
 while true; do
     find ~/.i3/wallpapers -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
-        shuf -n1 -z | xargs -0 feh --bg-max
+        shuf -n1 -z | xargs -0 feh --bg-fill
     sleep 10s
 done
